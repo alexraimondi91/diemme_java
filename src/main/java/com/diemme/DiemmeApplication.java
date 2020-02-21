e6d459a1689216c474717b85a4644089afad7df2
Alessandro <alex_994@ymail.com>
2020-02-21 23:01:53 +0100

    Primo fix

--- src/main/java/com/diemme/DiemmeApplication.java
+++ src/main/java/com/diemme/DiemmeApplication.java
@@ -1,11 +1,13 @@
-package com.diemme.application;
+package com.diemme;
 
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan("com.diemme")
 public class DiemmeApplication {
 
 	public static void main(String[] args) {
