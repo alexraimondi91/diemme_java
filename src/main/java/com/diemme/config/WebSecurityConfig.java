d747d870c7dac1ed832f94aaed36bddf93f92509
Alessandro <alex_994@ymail.com>
2020-05-01 19:37:08 +0200

    Lite login

--- src/main/java/com/diemme/config/WebSecurityConfig.java
+++ src/main/java/com/diemme/config/WebSecurityConfig.java
@@ -2,27 +2,45 @@ package com.diemme.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserDetailsService userDetailsService;
 
-    @Override
+	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+		auth.userDetailsService(userDetailsService);
 	}
 
 	@Bean
-	public PasswordEncoder passwordEncoder() {
+	public String passwordEncoder() {
 
-		return new BCryptPasswordEncoder();
+		return "password";
+
+	}
+
+	
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.headers().disable().csrf().disable().formLogin().loginPage("/login").loginProcessingUrl("/login")
+				.failureUrl("/?error=invalidlogin").defaultSuccessUrl("/", false).and().logout()
+				.logoutSuccessUrl("/").and().exceptionHandling().accessDeniedPage("/login").and()
+				.authorizeRequests()
+				// Specificare le url che sono soggette ad autenticazione ed autorizzazione
+				.antMatchers("/login", "/static/", "/favicon.ico").permitAll().antMatchers("/common/").authenticated()
+				.antMatchers("/prodotti/", "/ssds/").hasAnyRole("admin")
+				.antMatchers("/preventivi/", "/ssds/").hasAnyRole("client");
+
 
 	}
 
