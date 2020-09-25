5fc6288c595b9ac5ff9b737bf4427b8bb19a4509
Alessandro <alessandro.sallese@student.univaq.it>
2020-09-25 18:20:17 +0200

    Update UI

--- src/main/java/com/diemme/domain/mysql/Chat.java
+++ src/main/java/com/diemme/domain/mysql/Chat.java
@@ -1,23 +1,18 @@
 package com.diemme.domain.mysql;
 
 
-import java.time.ZonedDateTime;
 import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
-import org.hibernate.annotations.CreationTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
